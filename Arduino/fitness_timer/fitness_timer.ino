eed381f806968d8f219296611730fe99adfb7db9
weigu1 <weigu@weigu.lu>
2023-03-06 12:01:00 +0100

    added some comments

--- Arduino/fitness_timer/fitness_timer.ino
+++ Arduino/fitness_timer/fitness_timer.ino
@@ -1,12 +1,47 @@
 /*
- * fitness_timer.ino   v1.0
- * weigu.lu
- * LOLIN (Wemos) D1 mini pro with Wemos OLED display (new version with buttons)
- * Rotary encoder: on D5, D6, D7
- * use forked lib for OLED display on https://github.com/weigu1/Adafruit_SSD1306_Wemos_OLED
- * Use forked encoder lib from here: https://github.com/weigu1/Encoder
- * Download zip files and add with "Sketch -> Include Library -> Add .ZIP Library" 
- */
+  fitness_timer.ino
+
+  Version 1.1 2023-03-04
+   
+  LOLIN (Wemos) D1 mini pro with Wemos OLED display (new version with buttons)
+  Rotary encoder: on D5, D6, D7
+  use forked lib for OLED display on https://github.com/weigu1/Adafruit_SSD1306_Wemos_OLED
+  Use forked encoder lib from here: https://github.com/weigu1/Encoder
+  Use wemos lib from here: https://github.com/wemos/LOLIN_OLED_I2C_Button_Library
+  Download zip files and add with "Sketch -> Include Library -> Add .ZIP Library" 
+ 
+ ---------------------------------------------------------------------------
+  Copyright (C) 2023 Guy WEILER www.weigu.lu
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  ---------------------------------------------------------------------------
+ 
+  ESP8266: LOLIN/WEMOS D1 mini pro
+  ESP32:   MH ET LIVE ESP32-MINI-KIT 
+  
+  MHET    | MHET    - LOLIN        |---| LOLIN      - MHET    | MHET
+  
+  GND     | RST     - RST          |---| TxD        - RxD(3)  | GND
+   NC     | SVP(36) -  A0          |---| RxD        - TxD(1)  | 27 
+  SVN(39) | 26      -  D0(16)      |---|  D1(5,SCL) -  22     | 25
+   35     | 18      -  D5(14,SCK)  |---|  D2(4,SDA) -  21     | 32
+   33     | 19      -  D6(12,MISO) |---|  D3(0)     -  17     | TDI(12)
+   34     | 23      -  D7(13,MOSI) |---|  D4(2,LED) -  16     | 4
+  TMS(14) | 5       -  D8(15,SS)   |---| GND        - GND     | 0   
+   NC     | 3V3     - 3V3          |---|  5V        -  5V     | 2
+  SD2(9)  | TCK(13)                |---|              TD0(15) | SD1(8)
+  CMD(11) | SD3(10)                |---|              SD0(7)  | CLK(6)
+*/
 
 #include <Wire.h>
 #include <Adafruit_GFX.h>
